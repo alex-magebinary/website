ec552882be4ac20f780d557944497cb2b18e937c
reinka <andrei.poehlmann90@gmail.com>
2017-03-06 17:45:50 +0100

    [REF] Update tour

--- website_sale_one_step_checkout/static/src/js/website_sale_one_step_checkout_tour.js
+++ website_sale_one_step_checkout/static/src/js/website_sale_one_step_checkout_tour.js
@@ -1,60 +1,62 @@
-// (function () {
-//     'use strict';
-//
-//     odoo.Tour.register({
-//         id:   'shop_buy_product_oca',
-//         name: "Try to buy products with one step checkout",
-//         path: '/shop',
-//         mode: 'test',
-//         steps: [
-//             {
-//                 //step 0
-//                 title:    "select iPad Mini",
-//                 element: 'a[itemprop="name"][href*="ipad-mini"]',
-//             },
-//             {
-//                 //step 1
-//                 title:    "click on add to cart",
-//                 element: '#add_to_cart',
-//             },
-//             {
-//                 //step 2
-//                 title:    "go to checkout",
-//                 element: 'a[href="/shop/checkout"]',
-//             },
-//             {
-//                 //step 3
-//                 title:     "test with input error",
-//                 element:   'form[action="/payment/transfer/feedback"] .btn[type="submit"]',
-//                 onload: function (tour) {
-//                     $("input[name='phone']").val("");
-//                 }
-//             },
-//             {
-//                 //step 5
-//                 title:     "test without input error",
-//                 waitFor:   'div[id="osc_billing"] .has-error',
-//                 element:   'form[action="/payment/transfer/feedback"] .btn[type="submit"]',
-//                 onload: function (tour) {
-//                     if ($("input[name='name']").val() === "")
-//                         $("input[name='name']").val("website_sale-test-shoptest");
-//                     if ($("input[name='email']").val() === "")
-//                         $("input[name='email']").val("website_sale_test_shoptest@websitesaletest.optenerp.com");
-//                     $("input[name='phone']").val("123");
-//                     $("input[name='street']").val("xyz");
-//                     $("input[name='city']").val("Magdeburg");
-//                     $("input[name='zip']").val("39104");
-//                     $("select[name='country_id']").val("11");
-//                 },
-//             },
-//
-//             {
-//                 //step 6
-//                 title:     "finish",
-//                 waitFor:   '.oe_website_sale:contains("Thank you for your order")',
-//             }
-//         ]
-//     });
-//
-//
-// }());
+odoo.define("website_sale_one_step_checkout.tour_shop", function (require) {
+    "use strict";
+
+    var core = require("web.core");
+    var tour = require("web_tour.tour");
+    var base = require("web_editor.base");
+
+    var _t = core._t;
+    console.log('tour register');
+    tour.register('shop_buy_product_oca', {
+
+        // name: "Try to buy products with one step checkout",
+        url: '/shop',
+        test: true,
+        wait_for: base.ready(),
+        },
+        [
+            {
+                // step 0
+                content: _t("select iPad Mini"),
+                trigger: 'a[itemprop="name"][href*="ipad-mini"]',
+                // position: "bottom",
+            }, {
+                //step 1
+                content: _t("click on add to cart"),
+                trigger: '#add_to_cart',
+                // position: 'bottom',
+            }, {
+                //step 2
+                content:    _t("go to checkout"),
+                trigger: 'a[href="/shop/checkout"]',
+            }, {
+                //step 3
+                content:     _t("test with input error"),
+                trigger:   'form[action="/payment/transfer/feedback"] .btn[type="submit"]',
+                onload: function (tour) {
+                    $("input[name='phone']").val("");
+                }
+            }, {
+                //step 5
+                content:     _t("test without input error"),
+                waitFor:   'div[id="osc_billing"] .has-error',
+                trigger:   'form[action="/payment/transfer/feedback"] .btn[type="submit"]',
+                onload: function (tour) {
+                    if ($("input[name='name']").val() === "")
+                        $("input[name='name']").val("website_sale-test-shoptest");
+                    if ($("input[name='email']").val() === "")
+                        $("input[name='email']").val("website_sale_test_shoptest@websitesaletest.optenerp.com");
+                    $("input[name='phone']").val("123");
+                    $("input[name='street']").val("xyz");
+                    $("input[name='city']").val("Magdeburg");
+                    $("input[name='zip']").val("39104");
+                    $("select[name='country_id']").val("11");
+                },
+            }, {
+                //step 6
+                content:     _t("finish"),
+                waitFor:   '.oe_website_sale:contains("Thank you for your order")',
+            }
+        ]
+    );
+});
--- website_sale_one_step_checkout/tests/test_sale_osc_process.py
+++ website_sale_one_step_checkout/tests/test_sale_osc_process.py
@@ -8,18 +8,15 @@ import odoo.tests
 @odoo.tests.common.post_install(True)
 class TestUi(odoo.tests.HttpCase):
     def test_01_admin_checkout(self):
-        self.phantom_js("/", "odoo.Tour.run('shop_buy_product_oca',"
-                             "" "'test')",
-                        "odoo.Tour.tours.shop_buy_product_oca",
+        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop_buy_product_oca')",
+                        "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_buy_product_oca.ready",
                         login="admin")
 
     def test_02_demo_checkout(self):
-        self.phantom_js("/", "odoo.Tour.run('shop_buy_product_oca', "
-                             "'test')",
-                        "odoo.Tour.tours.shop_buy_product_oca",
+        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop_buy_product_oca')",
+                        "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_buy_product_oca.ready",
                         login="demo")
 
     def test_03_public_checkout(self):
-        self.phantom_js("/", "odoo.Tour.run('shop_buy_product_oca', "
-                             "'test')",
-                        "odoo.Tour.tours.shop_buy_product_oca")
+        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop_buy_product_oca')",
+                             "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_buy_product_oca.ready")
