c7f6ae3049b0b70b0a58e086ab8838a626a573d2
reinka <andrei.poehlmann90@gmail.com>
2017-03-07 17:03:05 +0100

    [REF] migrate tour for demo & public user

--- website_sale_one_step_checkout/static/src/js/website_sale_one_step_checkout_tour.js
+++ website_sale_one_step_checkout/static/src/js/website_sale_one_step_checkout_tour.js
@@ -7,9 +7,9 @@ odoo.define("website_sale_one_step_checkout.tour_shop", function (require) {
 
     var _t = core._t;
     console.log('tour register');
-    tour.register('shop_buy_product_oca', {
+    tour.register('shop_buy_product_oca_demo', {
 
-        // name: "Try to buy products with one step checkout",
+        name: "Buy products with the one-step-checkout",
         url: '/shop',
         test: true,
         wait_for: base.ready(),
@@ -17,45 +17,118 @@ odoo.define("website_sale_one_step_checkout.tour_shop", function (require) {
         [
             {
                 // step 0
-                content: _t("select iPad Mini"),
+                content: _t("Select iPad Mini"),
                 trigger: 'a[itemprop="name"][href*="ipad-mini"]',
                 // position: "bottom",
             }, {
                 //step 1
-                content: _t("click on add to cart"),
+                content: _t("Click on 'Add to cart'"),
                 trigger: '#add_to_cart',
                 // position: 'bottom',
             }, {
                 //step 2
-                content:    _t("go to checkout"),
-                trigger: 'a[href="/shop/checkout"]',
+                content:    _t("Proceed to checkout"),
+                // need regex, german version: /de_DE/shop/checkout
+                trigger: 'a[href$="/shop/checkout"]',
             }, {
                 //step 3
-                content:     _t("test with input error"),
-                trigger:   'form[action="/payment/transfer/feedback"] .btn[type="submit"]',
-                onload: function (tour) {
-                    $("input[name='phone']").val("");
+                content:     _t("Add new shipping address"),
+                trigger:   '#add-shipping-address .btn',
+            },  {
+                //step 3
+                content:     _t("Test with input error"),
+                waitFor: '.modal-body.address-form #osc-modal-form',
+                trigger:   '#js_confirm_address.btn',
+                run: function () {
+                    $("input[name='name']").val("website_sale-test-shoptest");
                 }
             }, {
-                //step 5
-                content:     _t("test without input error"),
+                //step 4
+                content:     _t("Test without input error"),
                 waitFor:   'div[id="osc_billing"] .has-error',
-                trigger:   'form[action="/payment/transfer/feedback"] .btn[type="submit"]',
-                onload: function (tour) {
-                    if ($("input[name='name']").val() === "")
-                        $("input[name='name']").val("website_sale-test-shoptest");
+                trigger:   '#js_confirm_address.btn',
+                run: function () {
+                    if ($("input[name='name']").val() === ""){
+                        $("input[name='name']").val("website_sale-test-shoptest");}
                     if ($("input[name='email']").val() === "")
                         $("input[name='email']").val("website_sale_test_shoptest@websitesaletest.optenerp.com");
                     $("input[name='phone']").val("123");
                     $("input[name='street']").val("xyz");
                     $("input[name='city']").val("Magdeburg");
                     $("input[name='zip']").val("39104");
-                    $("select[name='country_id']").val("11");
+                    $("select[name='country_id']").val("58");
                 },
             }, {
+            // step 5 TODO: Possible to avoid doing this twice?
+            content: _t('Confirm address'),
+            trigger:   '#js_confirm_address.btn',
+            }, {
+                // step 6
+                content: _t('Confirm payment and wait for redirection to Confirmation page'),
+                trigger: '.js_payment .btn'
+            }
+            // // , {
+            //     step 7
+            //     content:     _t("Finish"),
+            //     // waitFor:   '.oe_website_sale:contains("Pending")'
+            // }
+        ]
+    );
+
+    tour.register('shop_buy_product_oca_public', {
+
+        name: "Buy products with the one-step-checkout",
+        url: '/shop',
+        test: true,
+        wait_for: base.ready(),
+        },
+        [
+            {
+                // step 0
+                content: _t("Select iPad Mini"),
+                trigger: 'a[itemprop="name"][href*="ipad-mini"]',
+                // position: "bottom",
+            }, {
+                //step 1
+                content: _t("Click on 'Add to cart'"),
+                trigger: '#add_to_cart',
+                // position: 'bottom',
+            }, {
+                //step 2
+                content:    _t("Proceed to checkout"),
+                // need regex, german version: /de_DE/shop/checkout
+                trigger: 'a[href$="/shop/checkout"]',
+            }, {
+                //step 3
+                content:     _t("Test with input error"),
+                trigger:   '#js_confirm_address.btn',
+                run: function () {
+                    $("input[name='name']").val("website_sale-test-shoptest");
+                }
+            }, {
+                //step 4
+                content:     _t("Test without input error"),
+                waitFor:   'div[id="osc_billing"] .has-error',
+                trigger:   '#js_confirm_address.btn',
+                run: function () {
+                    if ($("input[name='name']").val() === ""){
+                        $("input[name='name']").val("website_sale-test-shoptest");}
+                    if ($("input[name='email']").val() === "")
+                        $("input[name='email']").val("website_sale_test_shoptest@websitesaletest.optenerp.com");
+                    $("input[name='phone']").val("123");
+                    $("input[name='street']").val("xyz");
+                    $("input[name='city']").val("Magdeburg");
+                    $("input[name='zip']").val("39104");
+                    $("select[name='country_id']").val("58");
+                },
+            }, {
+            // step 5 TODO: Possible to avoid doing this twice?
+            content: _t('Confirm address'),
+            trigger:   '#js_confirm_address.btn',
+            }, {
                 //step 6
-                content:     _t("finish"),
-                waitFor:   '.oe_website_sale:contains("Thank you for your order")',
+                content: _t('Confirm payment and wait for redirection to Confirmation page'),
+                trigger: '.js_payment .btn'
             }
         ]
     );
