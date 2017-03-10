c0cce5d706eb2fc1824e1287540fa2ff46ffca11
reinka <andrei.poehlmann90@gmail.com>
2017-03-10 20:02:19 +0100

    [REM] Terms & Condition + Country fields

--- website_sale_one_step_checkout/models/website.py
+++ website_sale_one_step_checkout/models/website.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2015 bloopark systems (<http://bloopark.de>)
+# © 2017 bloopark systems (<http://bloopark.de>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import fields, models, api
 
@@ -11,11 +11,3 @@ class Website(models.Model):
     use_osc = fields.Boolean(
         string='Use OSC',
         default=True,)
-    use_all_checkout_countries = fields.Boolean(
-        string='Use All Countries in Checkout',
-        default=True, )
-    checkout_country_ids = fields.Many2many(
-        'res.country',
-        'checkout_country_rel',
-        'website_id', 'country_id',
-        'Checkout Countries')
